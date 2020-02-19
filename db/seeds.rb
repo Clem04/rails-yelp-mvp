1f7338756e42120c8787b651e67eb9c3190dd10b
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-19 11:37:59 -0500

    Added data to test app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  { name: 'Dishoom', address: '7 Boundary St, London E2 7JE',
+    phone: 450_886_976, stars: 5 },
+  { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ',
+    phone: 514_665_473, stars: 4 },
+  { name: 'Dishoom', address: '7 Boundary St, London E2 7JE',
+    phone: 438_746_999, stars: 5 },
+  { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ',
+    phone: 450_888_765, stars: 4 }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
